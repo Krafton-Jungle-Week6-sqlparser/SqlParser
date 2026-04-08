a57d4980cde36724c778dda5ec28032c2e56eebb
kda <1325680q@gmail.com>
2026-04-08 13:44:41 +0000

    Update student sample data and insert example

--- examples/insert_users.sql
+++ examples/insert_users.sql
@@ -1,2 +1,2 @@
-INSERT INTO student (id, department, student_number, name, age) VALUES (6, '소프트웨어학과', 2024006, '한지우', 21);
+INSERT INTO student (id, department, student_number, name, age) VALUES (5,'물리학과',2026004,'정영훈',20);
 
